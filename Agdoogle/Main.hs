417e7a6ceef4ecfdf68b736f00d8a6d65fb1a780
Frank <frank0012@live.com>
2023-03-27 11:39:13 +0100

    wsl utf8

--- Agdoogle/Main.hs
+++ Agdoogle/Main.hs
@@ -1,33 +1,33 @@
 module Main where
 
---import Agdoogle
+import Agdoogle
 import Main.Utf8 (withUtf8)
-import Debug.Trace
-import qualified Data.Text.IO as TIO
-import Data.Text as DT
-import System.IO
-import qualified System.IO.Utf8 as Utf8
+--import Debug.Trace
+--import qualified Data.Text.IO as TIO
+--import Data.Text as DT
+--import System.IO
+--import qualified System.IO.Utf8 as Utf8
 
-writeData :: Handle -> Text -> IO ()
-writeData hOut inData = Utf8.withHandle hOut $ do
-  TIO.hPutStr hOut inData
+--writeData :: Handle -> Text -> IO ()
+--writeData hOut inData = Utf8.withHandle hOut $ do
+--  TIO.hPutStr hOut inData
 
---main :: IO ()
---main = withUtf8 $ do
---    agdoogle
-
-main :: IO () 
+main :: IO ()
 main = withUtf8 $ do
-    line <- System.IO.getLine
-    replaceType line
+    agdoogle
 
-replaceType :: String -> IO ()
-replaceType type' = withUtf8 $ do
-  contents <- TIO.readFile "AgdaDatabase/searchTerm.agda"
-  let modifiedContents = DT.unlines $ Prelude.map replaceLine (DT.lines contents)
-  withFile "AgdaDatabase/searchTerm.agda" WriteMode $ \handle -> do
-    writeData handle (DT.pack type')
-  where
-    replaceLine line
-      | DT.isInfixOf (DT.pack ":") line = DT.pack ("searchTerm : " ++ type')
-      | otherwise = line
+--main :: IO () 
+--main = withUtf8 $ do
+--    line <- System.IO.getLine
+--    replaceType line
+
+--replaceType :: String -> IO ()
+--replaceType type' = withUtf8 $ do
+--  contents <- TIO.readFile "AgdaDatabase/searchTerm.agda"
+--  let modifiedContents = DT.unlines $ Prelude.map replaceLine (DT.lines contents)
+--  withFile "AgdaDatabase/searchTerm.agda" WriteMode $ \handle -> do
+--    writeData handle (DT.pack type')
+--  where
+--    replaceLine line
+--      | DT.isInfixOf (DT.pack ":") line = DT.pack ("searchTerm : " ++ type')
+--      | otherwise = line
